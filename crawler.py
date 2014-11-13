b7634d1e3e81b4ded6f6ba5193cd838a8b204baa
lijiajia <lijiajia_1010@126.com>
2014-11-13 11:32:44 +0800

    add BeautifulSoup

--- crawler.py
+++ crawler.py
@@ -2,11 +2,18 @@
 # coding=utf-8
 
 import urllib2
+from BeautifulSoup import BeautifulSoup
 
 
 def main():
-    html = urllib2.urlopen('http://baidu.com').read()
+    html = urllib2.urlopen('http://tool.httpcn.com/KangXi/BuShou.html').read()
     print html
+    soup = BeautifulSoup(html)
+    title = soup.title
+    print type(title)
+    print title.name
+    print title.string
+#    print soup.find_all('a')
 
 
 if __name__ == '__main__':
