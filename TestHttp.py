aa788681c16a96bdf211a916464e82249fa582bc
lijiajia <lijiajia_1010@126.com>
2014-11-13 15:12:26 +0800

    commit

--- TestHttp.py
+++ TestHttp.py
@@ -1,8 +1,8 @@
 import httplib
 
 
-conn = httplib.HTTPConnection('test.gewala.net')
-conn.request("GET", "/openapi2/router/rest?appkey=xiaomitv&format=xml&method=com.gewara.partner.movie.opiSeatList&sign=C8E4C7F641817C53B323F0995E92D896&signmethod=MD5&timestamp=2014-08-26%2009:11:44&v=1.0&mpid=36087373")
+conn = httplib.HTTPConnection('tool.httpcn.com')
+conn.request("GET", "/Html/KangXi/BuShou/1_1.html")
 r = conn.getresponse()
 print r.status, r.reason
 
--- crawler.py
+++ crawler.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import urllib2
+import re
 from BeautifulSoup import BeautifulSoup
+
+
+BASE_DIR = 'http://tool.httpcn.com'
+
+
+def get_character(url):
+    path = BASE_DIR + url
+    html = urllib2.urlopen(path).read().decode('utf-8')
+    print '============================================================'
+    print html
+    print '============================================================'
+    soup = BeautifulSoup(html)
+    character_list = soup.findAll(href=re.compile('^/Html/KangXi/*'))
+    for character in character_list:
+        print character
 
 
 def main():
     html = urllib2.urlopen('http://tool.httpcn.com/KangXi/BuShou.html').read()
     print html
     soup = BeautifulSoup(html)
-    title = soup.title
-    print type(title)
-    print title.name
-    print title.string
-#    print soup.find_all('a')
+    bushou_list = soup.findAll(href=re.compile('^/Html/KangXi/*'))
+    for bushou in bushou_list:
+        href = bushou['href']
+        get_character(href)
+        return 0
 
 
 if __name__ == '__main__':
